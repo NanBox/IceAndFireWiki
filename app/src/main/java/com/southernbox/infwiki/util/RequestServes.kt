d85f8d739707c5a441213c498c6510e67e840059
LinNanQuan <linnq@zdnst.com>
2017-08-08 17:45:01 +0800

    修复获取图片的接口

--- app/src/main/java/com/southernbox/infwiki/util/RequestServes.kt
+++ app/src/main/java/com/southernbox/infwiki/util/RequestServes.kt
@@ -33,7 +33,7 @@ interface RequestServes {
     fun getCategoryMembers(@Query("cmtitle") title: String, @Query("cmcontinue") cmcontinue: String): Call<WikiResponse>
 
     //获取页面封面
-    @GET("api.php?action=query&prop=pageimages&format=json&pithumbsize=300&pilimit=20")
+    @GET("api.php?action=query&prop=pageimages&format=json&pithumbsize=300&pilicense=any&pilimit=20")
     fun getPageImages(@Query("titles") titles: String): Call<String>
 
     //全文搜索
